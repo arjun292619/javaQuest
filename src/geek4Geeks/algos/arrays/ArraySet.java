5e31a4d5b27e942140f2d86c5441a0efdd23e465
arjun292619 <j.c.arjunreddy@gmail.com>
2025-06-13 21:04:07 -0700

    Recursion tower of hanoi

---
+++ src/geek4Geeks/algos/arrays/ArraySet.java
@@ -0,0 +1,19 @@
+package geek4Geeks.algos.arrays;
+
+public class ArraySet {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] reverseArray(int[] numbers) {
+        return null;
+    }
+
+    public int maxElement(int[] numbers) {
+        return 0;
+    }
+
+    public int secondMaxElement(int[] numbers) {
+        return 0;
+    }
+}
--- src/geek4Geeks/algos/recursion/RecursionSet.java
+++ src/geek4Geeks/algos/recursion/RecursionSet.java
@@ -11,6 +11,9 @@ public class RecursionSet {
         System.out.println(lineseparator);
         printOne2N(n);
         System.out.println();
+        System.out.println(lineseparator);
+        n = 3;
+        towerOfHanoi(3, 'A', 'C', 'B');
     }
 
     public static void printN2One(int n) {
@@ -23,5 +26,16 @@ public class RecursionSet {
         if (n < 1) return;
         printOne2N(n - 1);
         System.out.print(n + " ");
+    }
+
+    public static void towerOfHanoi(int n, char start, char end, char aux) {
+        if (n == 0) return;
+        towerOfHanoi(n - 1, start, aux, end);
+        System.out.println("moving disk" + n + " from " + start + " to " + end);
+        towerOfHanoi(n - 1, aux, end, start);
+    }
+
+    public static int joshephus(int n, int k) {
+        return 0;
     }
 }
