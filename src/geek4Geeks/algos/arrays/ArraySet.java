ea724f5186754c5b86d0085b0a0ad72112ded97e
arjun292619 <j.c.arjunreddy@gmail.com>
2025-06-14 23:10:50 -0700

    ArraySet: max element

--- src/geek4Geeks/algos/arrays/ArraySet.java
+++ src/geek4Geeks/algos/arrays/ArraySet.java
@@ -1,16 +1,31 @@
 package geek4Geeks.algos.arrays;
 
+import java.util.Arrays;
+
 public class ArraySet {
     public static void main(String[] args) {
-
+        String lineseparator = "-".repeat(30);
+        System.out.println(lineseparator);
+        int[] arr = {20, 10, 20, 4, 100};
+        System.out.println("max element in arr " + Arrays.toString(arr) + ": " + maxElement(arr));
     }
 
     public int[] reverseArray(int[] numbers) {
         return null;
     }
 
-    public int maxElement(int[] numbers) {
-        return 0;
+    public int[] reverseSubArray(int[] numbers, int start, int end) {
+        return null;
+    }
+
+    public static int maxElement(int[] numbers) {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
     }
 
     public int secondMaxElement(int[] numbers) {
