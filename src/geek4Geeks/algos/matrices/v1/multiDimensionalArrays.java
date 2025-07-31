eff77639f33ec745b7e8f660b61a758016c2fa85
arjun292619 <j.c.arjunreddy@gmail.com>
2025-07-31 08:44:24 -0700

    matrix algos

---
+++ src/geek4Geeks/algos/matrices/v1/multiDimensionalArrays.java
@@ -0,0 +1,41 @@
+package geek4Geeks.algos.matrices.v1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class multiDimensionalArrays {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 7, 8}, {4, 5, 6}, {9, 10, 11, 12, 13, 14, 15}};
+        int[][] multarr1 = new int[3][4];
+        int[][] jaggedArr = new int[3][];
+
+        for (int i = 0; i < multarr1.length; i++) {
+            for (int j = 0; j < multarr1[i].length; j++) {
+                multarr1[i][j] = new Random().nextInt(0, 26);
+            }
+        }
+
+        for (int i = 0; i < jaggedArr.length; i++) {
+            jaggedArr[i] = new int[i + 3];
+            for (int j = 0; j < jaggedArr[i].length; j++) {
+                Arrays.fill(jaggedArr[i], new Random().nextInt(0, 26));
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("%-5d", arr[i][j]);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < multarr1.length; i++) {
+            for (int j = 0; j < multarr1[i].length; j++) {
+                System.out.printf("%-5d", multarr1[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.deepToString(jaggedArr));
+
+    }
+}
