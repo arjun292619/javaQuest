ea6ee64541be6d79b4e57b8e542c51989ff20feb
arjun292619 <j.c.arjunreddy@gmail.com>
2025-08-01 09:24:31 -0700

    more matrix traversal algos

--- src/geek4Geeks/algos/matrices/v1/multiDimensionalArrays.java
+++ src/geek4Geeks/algos/matrices/v1/multiDimensionalArrays.java
@@ -5,9 +5,13 @@ import java.util.Random;
 
 public class multiDimensionalArrays {
     public static void main(String[] args) {
+        String ls = "-".repeat(50);
         int[][] arr = {{1, 2, 3, 7, 8}, {4, 5, 6}, {9, 10, 11, 12, 13, 14, 15}};
+        int[][] rowArr = {{1, 2, 3, 4}};
+        int[][] colArr = {{1}, {2}, {3}, {4}};
         int[][] multarr1 = new int[3][4];
         int[][] jaggedArr = new int[3][];
+        int[][] sqArr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 
         for (int i = 0; i < multarr1.length; i++) {
             for (int j = 0; j < multarr1[i].length; j++) {
@@ -33,7 +37,24 @@ public class multiDimensionalArrays {
         System.out.println("Print the jagged Array");
         printArray(jaggedArr);
 
+//        print snake case pattern
+        System.out.println("Print Boundary Traversal");
+        printSnakePattern(sqArr);
+        printSnakePattern(arr);
+        System.out.println(ls);
+
+//        print boundary traversal
+
+        printArray(sqArr);
+        System.out.println("Print Boundary Traversal");
+        printBoundary(sqArr);
+        System.out.println();
+        printBoundary(rowArr);
+        System.out.println();
+        printBoundary(colArr);
+
     }
+
 
     private static void printArray(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -42,5 +63,48 @@ public class multiDimensionalArrays {
             }
             System.out.println();
         }
+    }
+
+    private static void printSnakePattern(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.printf("%-5d", arr[i][j]);
+                }
+            } else {
+                for (int j = arr[i].length - 1; j >= 0; j--) {
+                    System.out.printf("%-5d", arr[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printBoundary(int[][] arr) {
+        if (arr.length == 1) {
+            for (int i = 0; i < arr[0].length; i++) {
+                System.out.printf("%-5d", arr[0][i]);
+            }
+        } else if (arr[0].length == 1) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.printf("%-5d", arr[i][0]);
+            }
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.printf("%-5d", arr[0][i]);
+            }
+
+            for (int i = 1; i < arr[0].length; i++) {
+                System.out.printf("%-5d", arr[i][arr.length - 1]);
+            }
+
+            for (int i = arr.length - 2; i >= 0; i--) {
+                System.out.printf("%-5d", arr[arr.length - 1][i]);
+            }
+            for (int i = arr.length - 2; i >= 1; i--) {
+                System.out.printf("%-5d", arr[i][0]);
+            }
+        }
+
     }
 }
