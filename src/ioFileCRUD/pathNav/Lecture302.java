92e717f8316b38ff90ae2d430c4304bb7d4e427a
arjun292619 <j.c.arjunreddy@gmail.com>
2025-08-05 12:00:29 -0700

    more listing dir using walk and files

--- src/ioFileCRUD/pathNav/Lecture302.java
+++ src/ioFileCRUD/pathNav/Lecture302.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.stream.Stream;
 
 public class Lecture302 {
@@ -12,9 +14,22 @@ public class Lecture302 {
         System.out.println(lineSeparator);
         Path currPath = Path.of("");
         System.out.println("cwd: " + currPath.toAbsolutePath());
+
+        System.out.println("Listing using List");
         try (Stream<Path> paths = Files.list(currPath)) {
             paths.
                     map(Lecture302::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(lineSeparator);
+        System.out.println("Listing using walk");
+        try (Stream<Path> pathStream = Files.walk(currPath, 2)) {
+            pathStream
+                    .filter(Files::isRegularFile)
+                    .map(Lecture302::listDir)
                     .forEach(System.out::println);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -25,7 +40,9 @@ public class Lecture302 {
         try {
             boolean isDir = Files.isDirectory(path);
             FileTime dateField = Files.getLastModifiedTime(path);
-            return "%s %-15s %s".formatted(dateField, (isDir ? "<DIR>" : ""), path);
+            LocalDateTime localModDt = LocalDateTime.ofInstant(dateField.toInstant(), ZoneId.systemDefault());
+            return "%tD %tT %-5s %12s %s".formatted(localModDt, localModDt,
+                    (isDir ? "<DIR>" : ""), (isDir ? "" : Files.size(path)), path);
         } catch (IOException e) {
             System.out.println("There is error with the path provided: " + path + " = " + e.getMessage());
             return path.toString();
